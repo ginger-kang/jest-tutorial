d6cf070c4755b045502a6dbf89dbda499a15cc4f
ginger-kang <kdhoon07@gmail.com>
2021-07-08 02:57:04 +0900

    Jest example

---
+++ server/app.test.js
@@ -0,0 +1,11 @@
+describe("plus test", () => {
+  it("1+2", done => {
+    expect(1 + 2).toEqual(3);
+    done();
+  });
+
+  it("1+1", done => {
+    expect(1 + 1).toEqual(3);
+    done();
+  });
+});
